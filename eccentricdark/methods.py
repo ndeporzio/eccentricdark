4a9e8bf5a321de909629e7faac14dea20d79a141
Nicholas DePorzio <nicholasdeporzio@g.harvard.edu>
2023-04-08 14:51:56 -0400

    Repair distribution samplers.

--- eccentricdark/methods.py
+++ eccentricdark/methods.py
@@ -7,53 +7,47 @@ import scipy.interpolate
 
 def generate_invcdf(pdf, xmin, xmax, xscale): 
 
-    #calculate cdf
     if xscale=='log':
-        xvals = np.logspace(np.log10(xmin), np.log10(xmax), ed.invcdf_density_factor)
-        xmidpoints = (xvals[1:] + xvals[:-1]) / 2.
-        dxvals = np.diff(xvals)
+         xvals = np.linspace(np.log10(xmin), np.log10(xmax), ed.invcdf_density_factor)
+         xmidpoints = (xvals[1:] + xvals[:-1]) / 2.
+         dxvals = np.diff(xvals)
 
-        normalization = np.sum(pdf(xmidpoints)*dxvals)
-        normed_pdf = lambda x: pdf(x)/normalization   
+         normalization = np.sum(pdf(np.power(10., xmidpoints))*dxvals)
+         normed_pdf = lambda x: pdf(x)/normalization   
 
-        cdf_vals = np.zeros(len(xvals)+1)
-        cdf_vals[1:-1] = np.cumsum(normed_pdf(xmidpoints)*dxvals)
-        cdf_vals[-1] = 1.
+         cdf_vals = np.zeros(len(xvals)+1)
+         cdf_vals[1:-1] = np.cumsum(normed_pdf(np.power(10., xmidpoints))*dxvals)
+         cdf_vals[-1] = 1.
 
+         fullxvals = np.append(np.append(xvals[0], xmidpoints), xvals[-1])
 
-        fullxvals = np.append(np.append(xvals[0], xmidpoints), xvals[-1])
+         cdf_interp = scipy.interpolate.interp1d(fullxvals, cdf_vals)
+         log10invcdf_interp = scipy.interpolate.interp1d(cdf_vals, fullxvals)
+         invcdf_interp = lambda x: np.power(10., log10invcdf_interp(x))
 
-        cdf_interp = scipy.interpolate.interp1d(fullxvals, cdf_vals)
-
-        log10invcdf_interp = scipy.interpolate.interp1d(cdf_vals, fullxvals)
-        
-        invcdf_interp = lambda x: log10invcdf_interp(x)
+        ########################
 
     elif xscale=='linear': 
         normalization = scipy.integrate.quad(pdf, xmin, xmax)[0]
         normed_pdf = lambda x: pdf(x)/normalization
-        #size_xbasis = np.int(np.floor((xmax-xmin)*ed.invcdf_density_factor))
-        xeval = np.linspace(xmin, xmax, ed.invcdf_density_factor) 
+
+        xeval = np.linspace(xmin, xmax, 100)
 
         cdf_func = np.vectorize(lambda x: scipy.integrate.quad(normed_pdf, xmin, x)[0])
         cdf = cdf_func(xeval)
-        cdf_interp = scipy.interpolate.interp1d(xeval, cdf) 
-
-        invcdf_interp = scipy.interpolate.interp1d(cdf, xeval) 
-
-    elif xscale == 'test':
-        normalization = scipy.integrate.quad(pdf, xmin, xmax)[0]
-        normed_pdf = lambda x: pdf(x)/normalization
-
-        xvals = np.logspace(np.log10(xmin), np.log10(xmax), ed.invcdf_density_factor)
-        cdf = lambda x: scipy.integrate.quad(normed_pdf, xmin, x)[0]
-        cdf_vals = np.vectorize(cdf)(xvals)
         
-        inv_cdf = scipy.interpolate.interp1d(cdf_vals, xvals)
-        invcdf_interp = lambda x: inv_cdf(x)
-        
-        plt.plot(np.log10(xvals), normed_pdf(xvals), label='True PDF')
+        deleteidxs = []
+        for idx, val in enumerate(cdf[:-1]): 
+            if ((val==cdf[idx+1]) and (val==0.)): 
+                deleteidxs.append(idx)
+            elif ((val==cdf[idx+1]) and (val==1.)):
+                deleteidxs.append(idx+1)
+                
+        xeval = np.delete(xeval, deleteidxs)
+        cdf = np.delete(cdf, deleteidxs)
 
+        cdf_interp = scipy.interpolate.interp1d(xeval, cdf)
+        invcdf_interp = scipy.interpolate.interp1d(cdf, xeval)
 
     return invcdf_interp
 
